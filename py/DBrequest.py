cd6656b902fa119c17fc598fe652ab1042420af3
Alexandre Borrel <alexandre.borrel@nih.gov>
2019-09-05 12:35:09 -0400

    Create class to interact with DB

--- py/DBrequest.py
+++ py/DBrequest.py
@@ -2,53 +2,63 @@ import psycopg2
 from configparser import ConfigParser
 
 
-def
+class DBrequest:
+    def __init__(self, verbose=1):
+        self.dbconfig = "database.ini"
+        self.conn = None
+        self.verbose = verbose
 
 
-def main():    
-    connect()
+    def config(self, section='postgresql'):
+        parser = ConfigParser()
+        parser.read(self.dbconfig)
+        dparams = {}
+        if parser.has_section(section):
+            params = parser.items(section)
+            for param in params:
+                dparams[param[0]] = param[1]
+        else:
+            raise Exception('Section {0} not found in the {1} file'.format(section, self.dbconfig))
+
+        self.params = dparams
 
 
-def connect():
-    conn = None
-    try:
-        # read connection parameters
-        params = config()
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params)
-        # create a cursor
-        cur = conn.cursor()              
-        cur.execute('select * from drugbank_chemicals  limit(10)')
-        print(cur)
-        line = cur.fetchall()
+    def connOpen(self):
+        try:
+            self.config()
+            if self.verbose: print('Connecting to the PostgreSQL database...')
+            self.conn = psycopg2.connect(** self.params)
 
-        print(line)
-        #or you can access array data as:
-        array = line[3]
-        data= array[2]
-        print(data)
-       # close the communication with the PostgreSQL
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
 
-def config(filename='database.ini', section='postgresql'):
-     parser = ConfigParser()
-     parser.read(filename)
-     db = {}
-     if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]]=param[1]
-     else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+    def connClose(self):
+        if self.conn is not None:
+            self.conn.close()
+            if self.verbose == 1: print('Database connection closed.')
 
-     return db
+    def execCMD(self, cmd):
 
-if __name__=="__main__":
-    main()
+        if self.conn != None:
+            try:
+                cur = self.conn.cursor()
+                cur.execute(cmd)
+                print(cur)
+                out = cur.fetchall()
+                if self.verbose == 1: print(out)
+            except (Exception, psycopg2.DatabaseError) as error:
+                print(error)
+        else:
+            print("Open connection first")
+
+
+
+cmd = 'select * from drugbank_chemicals  limit(10)'
+
+cmd = 'INSERT INTO drugbank_chemicals (drugbank_id, smiles_origin, smiles_clean, inchikey, qsar_ready)\n VALUES ("test1", "test2", "test3", "test4", false); '
+
+dbr = DBrequest()
+dbr.connOpen()
+dbr.execCMD(cmd)
+dbr.connClose()
+
